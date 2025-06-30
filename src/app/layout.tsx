db3fcbb08e829147e91063be51745c5766d790d6
Kim-kyuho <kgh9002@icloud.com>
2025-06-30 22:03:50 +0900

    Fixed header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`bg-background text-foreground font-extrabold antialiased`}>
         <Providers>
           <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
-            <Header />
-            <main className="p-4 sm:p-8 overflow-x-hidden">{children}</main>
+            <header className="fixed top-0 left-0 w-full z-50">
+              <Header />
+            </header>
+            <main className="pt-20 p-4 sm:p-8 overflow-x-hidden">{children}</main>
             <Footer />
           </div>
         </Providers>
