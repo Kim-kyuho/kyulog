05a5d21d2fd955aa268328dbe3214c0367b7c94f
Kim-kyuho <kgh9002@icloud.com>
2025-07-06 18:06:32 +0900

    add Analytics in Project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Providers from "./providers";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 
 export const metadata = {
